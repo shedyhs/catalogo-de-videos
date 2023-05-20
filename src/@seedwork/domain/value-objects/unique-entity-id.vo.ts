abb933bebd6363c72e668afa5b07d7d5db7cc637
shedyhs <shedyhs@gmail.com>
2023-05-20 19:13:37 +0000

    style: move errors folder to inside of @seedwork/domain

--- src/@seedwork/domain/value-objects/unique-entity-id.vo.ts
+++ src/@seedwork/domain/value-objects/unique-entity-id.vo.ts
@@ -1,5 +1,5 @@
 import { randomUUID } from 'crypto';
-import { InvalidUuidError } from '../../errors/invalid-uuid.error';
+import { InvalidUuidError } from '../errors/invalid-uuid.error';
 import { ValueObject } from './value-object';
 
 export class UniqueEntityId extends ValueObject<string> {
